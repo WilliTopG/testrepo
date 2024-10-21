68c82d5786a6a426a29b58c3fe82c69d52b6053e
WilliTopG <Wsbergum@gmail.com>
2024-10-21 11:53:44 +0200

    changed all

--- Hei verden.py
+++ Hei verden.py
@@ -1,11 +1,9 @@
-﻿# Mitt testrepo
-#%%
-
-import random
+﻿import random
 import string
 import time
 
 
+#%%
 def generate_hello_world():
     letters = string.ascii_letters
     target = "HelloWorld"
@@ -13,33 +11,36 @@ def generate_hello_world():
 
     for letter in target:
         while True:
-            I = random.choice(letters)
-            print(result + I)
-            if I == letter:
-                result += I
+            i = random.choice(letters)
+            print(result + i)
+            if i == letter:
+                result += i
                 break
             time.sleep(0.05)
+    return result
 
 
-    def generate_hello_world():
-    letters = string.ascii_letters
+#%%
+def generate_hello_world_custom():
+    letters = string.ascii_letters + " "
     target = "Oskar kom deg på stein kasting"
     result = ""
 
     for letter in target:
         while True:
-            I = random.choice(letters)
-            print(result + I)
-            if I == letter:
-                result += I
+            i = random.choice(letters)
+            print(result + i)
+            if i == letter:
+                result += i
                 break
             time.sleep(0.05)
     
     return result
 
-# Usage
-hello_world_result = generate_hello_world()
-        
-        
-        /fill  1 1 1  10 10 10  minecraft:stone
+if __name__ == "__main__":
+    print("Generating 'HelloWorld':")
+    print(generate_hello_world())
+    print("\nGenerating custom message:")
+    print(generate_hello_world_custom())
+
 
