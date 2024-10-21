01a0516b1f8d7f117b2078137b56da2b6456ebf5
WilliTopG <Wsbergum@gmail.com>
2024-10-21 10:40:38 +0200

    basic changes

--- Hei verden.py
+++ Hei verden.py
@@ -1,1 +1,25 @@
 ﻿# Mitt testrepo
+#%%
+
+import random
+import string
+import time
+
+
+
+letters = string.ascii_letters
+target = "HelloWorld"
+result = ""
+
+for letter in target:
+    while True:
+        I = random.choice(letters)
+        print(result + I)
+        if I == letter:
+            result += I
+            break
+        time.sleep(0.05)
+        
+        
+        /fill  1 1 1  10 10 10  minecraft:stone
+
